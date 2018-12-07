c9442c606015049b9ec200b508f2384ade321c3a
varHarrie <varharrie@gmail.com>
2018-12-07 11:12:30 +0800

    Improve tests

--- test/mock.test.ts
+++ test/mock.test.ts
@@ -1,30 +1,9 @@
-import Debug from 'debug'
 import { expect } from 'chai'
 
-import mock, { decorators } from '../src'
+import mock from '../src'
+import { Person, Superman } from './classes'
 
 describe('mock', () => {
-
-  class Person {
-
-    @decorators.string('lower', 5, 10)
-    public name: string
-
-    @decorators.integer(0, 100)
-    public age: number
-
-    public canFly: boolean = false
-
-  }
-
-  class Superman extends Person {
-
-    @decorators.integer(80, 100)
-    public power: number
-
-    public canFly: boolean = true
-
-  }
 
   it('mock(plain object)', () => {
     const r = mock({ name: 'Harrie', age: 18 })
@@ -37,7 +16,7 @@ describe('mock', () => {
     const r = mock({ name: mock.string('abc'), age: mock.natural(0, 100) })
 
     expect(r).to.have.property('name').is.a('string')
-    expect(r).to.have.property('age').is.a('number').least(0).below(100)
+    expect(r).to.have.property('age').is.a('number').least(0).most(100)
   })
 
   it('mock(object & object)', () => {
@@ -47,7 +26,7 @@ describe('mock', () => {
     )
 
     expect(r).to.have.property('name').is.a('string')
-    expect(r).to.have.property('age').is.a('number').least(0).below(100)
+    expect(r).to.have.property('age').is.a('number').least(0).most(100)
   })
 
   it('mock(object & array)', () => {
@@ -59,25 +38,25 @@ describe('mock', () => {
     const c = r.friends[0]
 
     expect(r).to.have.property('name').is.a('string')
-    expect(r).to.have.property('age').is.a('number').least(0).below(100)
+    expect(r).to.have.property('age').is.a('number').least(0).most(100)
     expect(r).to.have.property('friends').is.a('array').lengthOf(2)
 
     expect(c).to.have.property('name').is.a('string')
-    expect(c).to.have.property('age').is.a('number').least(0).below(100)
+    expect(c).to.have.property('age').is.a('number').least(0).most(100)
   })
 
   it('mock(class)', () => {
     const r = mock(Person)
 
     expect(r).to.have.property('name').is.a('string')
-    expect(r).to.have.property('age').is.a('number').least(0).below(100)
+    expect(r).to.have.property('age').is.a('number').least(0).most(100)
     expect(r).to.have.property('canFly').equal(false)
     expect(r).to.have.not.property('power')
 
     const r2 = mock(Superman)
     expect(r2).to.have.property('name').is.a('string')
-    expect(r2).to.have.property('age').is.a('number').least(0).below(100)
-    expect(r2).to.have.property('power').is.a('number').least(80).below(100)
+    expect(r2).to.have.property('age').is.a('number').least(0).most(100)
+    expect(r2).to.have.property('power').is.a('number').least(80).most(100)
     expect(r2).to.have.property('canFly').equal(true)
   })
 
@@ -85,9 +64,9 @@ describe('mock', () => {
     const r = mock(Person, { height: mock.integer(160, 180) })
 
     expect(r).to.have.property('name').is.a('string')
-    expect(r).to.have.property('age').is.a('number').least(0).below(100)
+    expect(r).to.have.property('age').is.a('number').least(0).most(100)
     expect(r).to.have.property('canFly').equal(false)
-    expect(r).to.have.property('height').is.a('number').least(160).below(180)
+    expect(r).to.have.property('height').is.a('number').least(160).most(180)
   })
 
   it('mock(class & array)', () => {
@@ -95,11 +74,11 @@ describe('mock', () => {
     const c = r.friends[0]
 
     expect(r).to.have.property('name').is.a('string')
-    expect(r).to.have.property('age').is.a('number').least(0).below(100)
+    expect(r).to.have.property('age').is.a('number').least(0).most(100)
     expect(r).to.have.property('friends').is.a('array').lengthOf(2)
 
     expect(c).to.have.property('name').is.a('string')
-    expect(c).to.have.property('age').is.a('number').least(0).below(100)
+    expect(c).to.have.property('age').is.a('number').least(0).most(100)
   })
 
 })
