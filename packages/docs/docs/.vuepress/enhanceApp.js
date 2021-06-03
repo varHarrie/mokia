38751c7c0d34b79e9945f31fb73ba3932d1312e1
varHarrie <varharrie@gmail.com>
2021-06-03 17:16:12 +0800

    chore: prepare for build

--- packages/docs/docs/.vuepress/enhanceApp.js
+++ packages/docs/docs/.vuepress/enhanceApp.js
@@ -1,12 +1,14 @@
-if (window) {
-  window.global = window.global || window;
-  window.process = window.process || { env: {} };
+export default ({ isServer }) => {
+  if (!isServer) {
+    window.global = window.global || window;
+    window.process = window.process || { env: {} };
 
-  window.mokia = require('mokia');
-  window.producer = require('@mokia/producer');
-  window.decorator = require('@mokia/decorator');
+    window.mokia = require('mokia');
+    window.producer = require('@mokia/producer');
+    window.decorator = require('@mokia/decorator');
 
-  console.log('mokia:', window.mokia);
-  console.log('producer:', window.producer);
-  console.log('decorator:', window.decorator);
-}
+    console.log('mokia:', window.mokia);
+    console.log('producer:', window.producer);
+    console.log('decorator:', window.decorator);
+  }
+};
