a940ef840204ef9608e95877f75ecf3f9c52ef54
varHarrie <varharrie@gmail.com>
2021-09-01 12:03:25 +0800

    fix: add defineConfig default generic type

--- packages/mokia/src/index.ts
+++ packages/mokia/src/index.ts
@@ -4,11 +4,11 @@ import type { ServerConfig } from '@mokia/server';
 
 type Mokia = typeof mock & typeof decorator & { producer: typeof producer; defineConfig: typeof defineConfig };
 
-type MokiaUserConfig<T = typeof producer> = ServerConfig<T>;
+type MokiaUserConfig<T> = ServerConfig<T>;
 
 const mock = <T>(schema: T) => producer.generate<T>(schema);
 
-const defineConfig = <T>(config: MokiaUserConfig<T>): MokiaUserConfig<T> => config;
+const defineConfig = <T = typeof producer>(config: MokiaUserConfig<T>): MokiaUserConfig<T> => config;
 
 const mokia: Mokia = Object.assign(mock, decorator, { producer, defineConfig });
 
